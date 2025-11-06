03b7acca47eedb93b5b6dc7c5275af581c46e294
armagansenol <armagansnl@gmail.com>
2025-11-06 13:33:59 +0300

    refactor: update footer layout to include address and improve styling for better readability

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -45,12 +45,19 @@ export function Footer() {
             ))}
           </div>
         </div>
-        <div className='flex flex-col-reverse lg:flex-row justify-between items-center gap-5 py-5'>
-          <span className='block w-full md:w-[70vw] lg:w-[50vw] xl:w-[35vw] text-center lg:text-left text-[0.8rem]'>
-            Citys Residences, HAYAT GAYRİMENKUL GELİŞTİRME TURİZM İNŞAAT VE TAAHHÜT A.Ş.&apos;nin tescilli bir
-            markasıdır. Markanın tüm kullanım hakları, operasyonel faaliyetleri ve temsiliyeti, ilgili muvafakatname
-            kapsamında CITYS GAYRİMENKUL GELİŞTİRME ANONİM ŞİRKETİ tarafından yürütülmektedir.
-          </span>
+        <div className='flex flex-col-reverse lg:flex-row justify-between items-center lg:items-start gap-5 py-5'>
+          <div className='lg:col-span-9 xl:col-span-8 text-center lg:text-left text-[0.8rem]'>
+            <span className='block text-center lg:text-left text-[0.8rem]'>
+              Kayışdağı Cad. No.2, Kozyatağı <br /> 34752 Ataşehir, İstanbul
+            </span>
+          </div>
+          <div className='lg:col-span-7 xl:col-span-6 flex flex-col items-center lg:flex-row justify-between gap-5'>
+            <span className='block w-full md:w-[70vw] lg:w-[46vw] xl:w-[42.5vw] 2xl:w-[32vw] text-center lg:text-left text-[0.8rem]'>
+              Citys Residences, HAYAT GAYRİMENKUL GELİŞTİRME TURİZM İNŞAAT VE TAAHHÜT A.Ş.&apos;nin tescilli bir
+              markasıdır. Markanın tüm kullanım hakları, operasyonel faaliyetleri ve temsiliyeti, ilgili muvafakatname
+              kapsamında CITYS GAYRİMENKUL GELİŞTİRME ANONİM ŞİRKETİ tarafından yürütülmektedir.
+            </span>
+          </div>
         </div>
       </div>
     </footer>
